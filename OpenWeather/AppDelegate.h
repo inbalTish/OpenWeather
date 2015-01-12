d54847db6aaeefb899885c1def684c8dabadc288
Dudi S <dudi@HenMac.local>
2015-01-12 15:03:28 +0200

    Initial Commit

---
+++ OpenWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenWeather
+//
+//  Created by Dudi S on 1/12/15.
+//  Copyright (c) 2015 Marat Ibragimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenWeather/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenWeather
+//
+//  Created by Dudi S on 1/12/15.
+//  Copyright (c) 2015 Marat Ibragimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
